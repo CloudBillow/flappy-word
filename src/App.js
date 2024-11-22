9b3a10c20db7b381d4ea3ddfcb49cf63cf41818f
XieYT <taotao772008@163.com>
2024-11-22 11:54:33 +0800

    feat: 添加版本号信息

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Game from './components/Game'
 import { GameProvider } from './context/GameContext'
 
 function App() {
+  // 版本信息
+  console.log(
+      '%c Flappy Bird v1.0.0',
+      'color: #fff; background: #6EBEC8; padding: 5px; border-radius: 5px'
+  )
   return (
       <div className="App">
         <GameProvider>
